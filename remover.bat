6982741d235b073a516d7fafd536c60150967c7d
AndroidPrior <48363233+AndroidPrior@users.noreply.github.com>
2019-03-08 23:59:55 -0500

    Add files via upload

---
+++ remover.bat
@@ -0,0 +1,80 @@
+@echo off
+
+REM Use this for educational purposes only.
+REM I do not any take responsability for you pranking your friends or getting in trouble.
+REM If you accidentally downloaded this or ran this, you can download the remover.bat from the gihub page. 
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+
+
+If EXIST C:\Users\%username%\AppData\Local\startup_copier.bat (
+	del "C:\Users\%username%\AppData\Local\startup_copier.bat"
+)
+ELSE(
+	ECHO hello
+)
+
+If EXIST C:\Users\%username%\AppData\Roaming\startup_copier2.bat (
+	del "C:\Users\%username%\AppData\Roaming\startup_copier2.bat"
+)
+ELSE(
+	ECHO hello
+)
+
+If EXIST C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat (
+	del "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat"
+)
+ELSE(
+	ECHO hello
+)
+
+If EXIST C:\Users\%username%\AppData\Roaming\Microsoft\msdfmap.bat (
+	del "C:\Users\%username%\AppData\Roaming\Microsoft\msdfmap.bat"
+)
+ELSE(
+	ECHO hello
+)
+
+
+If EXIST C:\Users\%username%\AppData\Roaming\Microsoft\msdfmap.vbs (
+	del "C:\Users\%username%\AppData\Roaming\Microsoft\msdfmap.vbs"
+)
+ELSE(
+	ECHO hello
+)
+
+
+If EXIST C:\Users\%username%\AppData\Roaming\Microsoft\mute.vbs (
+	del "C:\Users\%username%\AppData\Roaming\Microsoft\mute.vbs"
+)
+ELSE(
+	ECHO hello
+)
+
+
+
+If EXIST C:\Users\%username%\AppData\Roaming\Microsoft\update0.bat (
+	del "C:\Users\%username%\AppData\Roaming\Microsoft\update0.bat"
+)
+ELSE(
+	ECHO hello
+)
+
+
+If EXIST C:\Users\%username%\AppData\Local\startup.bat (
+	del "C:\Users\%username%\AppData\Local\startup.bat"
+)
+ELSE(
+	ECHO hello
+)
+
+
+
+

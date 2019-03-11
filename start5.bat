f853de2601e683b211a72db519154430a9d3e05c
AndroidPrior <48363233+AndroidPrior@users.noreply.github.com>
2019-03-11 00:01:59 -0400

    Create start5.bat

---
+++ start5.bat
@@ -0,0 +1,6 @@
+start http://www.linkbucks.com/Awxez
+start http://www.linkbucks.com/AwxqI
+start http://www.linkbucks.com/AwyAJ
+start http://www.linkbucks.com/AwylH
+start http://www.linkbucks.com/AwJ2o
+start http://www.linkbucks.com/Awim3

